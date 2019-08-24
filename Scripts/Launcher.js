ceba54dca86ffaa0ac7ebd71c2949dc25da9dba0
nimishb2000 <nimishb2000@gmail.com>
2019-08-24 17:10:07 +0530

    Level'

--- Scripts/Launcher.js
+++ Scripts/Launcher.js
@@ -1,19 +1,19 @@
 "use strict";
-document.getElementById('bird').addEventListener('mousedown', mouseDown);
-window.addEventListener('mouseup', mouseUp);
+function Bird_Launch(e){
+    var bird = document.getElementById('bird');
+    var new_x = e.clientX - 235;
+    var new_y = e.clientY - 619;
+    bird.style.left = new_x + 'px';
+    bird.style.top = new_y + 'px';
+}
 function mouseDown(){
-    window.addEventListener('mousemove', divMove, true);
+    window.addEventListener('mousemove', Bird_Launch, true);
 }
 function mouseUp(){
-    window.removeEventListener('mousemove', divMove, true);
-}
-function divMove(e){
-    var bird = document.getElementById('bird');
-    var new_x = e.clientX - 25;
-    var new_y = e.clientY - 25;
-    bird.style.marginLeft = new_x + 'px';
-    bird.style.marginTop = new_y + 'px';
+    window.removeEventListener('mousemove', Bird_Launch, true);
 }
 function gravity(){
     
 }
+document.getElementById('bird').addEventListener('mousedown', mouseDown);
+window.addEventListener('mouseup', mouseUp);
