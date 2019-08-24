4fa49f5b98779e5cad90ebbe48b6c4ce297048a2
nimishb2000 <nimishb2000@gmail.com>
2019-08-24 16:20:51 +0530

    Level 1

--- Scripts/Launcher.js
+++ Scripts/Launcher.js
@@ -1,20 +1,19 @@
 "use strict";
-var x, y;
 document.getElementById('bird').addEventListener('mousedown', mouseDown);
 window.addEventListener('mouseup', mouseUp);
 function mouseDown(){
-    x = event.clientX;
-    y = event.clientY;
     window.addEventListener('mousemove', divMove, true);
 }
 function mouseUp(){
     window.removeEventListener('mousemove', divMove, true);
 }
 function divMove(e){
-    var shiftX, shiftY;
     var bird = document.getElementById('bird');
     var new_x = e.clientX - 25;
     var new_y = e.clientY - 25;
     bird.style.marginLeft = new_x + 'px';
     bird.style.marginTop = new_y + 'px';
+}
+function gravity(){
+    
 }
