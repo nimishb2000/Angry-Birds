9a25276b5abe6a38bb664bb883e2c0c67cd73ad0
nimishb2000 <nimishb2000@gmail.com>
2019-08-25 14:33:18 +0530

    Projectile

--- Scripts/Launcher.js
+++ Scripts/Launcher.js
@@ -1,27 +1,21 @@
-document.getElementById('bird').addEventListener('mousedown', mouseDown);
+var disp, angle_deg, speed, height, angle, gravity_constant, new_x, new_y, range;
+gravity_constant = 20;
+var bird = document.getElementById('bird');
+var slingshot = document.getElementById('slingshot');
+bird.addEventListener('mousedown', mouseDown);
 window.addEventListener('mouseup', mouseUp);
 function mouseUp(){
-    window.removeEventListener('mousemove', divMove, true);
+    window.removeEventListener('mousemove', birdMove, true);
+    launch();
 }
 function mouseDown(){
-  window.addEventListener('mousemove', divMove, true);
+  window.addEventListener('mousemove', birdMove, true);
 }
-function getX(elmnt){
-    for(var x=0; elmnt != null; x += elmnt.offsetLeft, elmnt = elmnt.offsetParent);
-    return x;
-}
-function getY(elmnt){
-    for (var y=0; elmnt != null; y += elmnt.offsetTop, elmnt = elmnt.offsetParent);
-    return y;
-}
-function divMove(e){
-    var new_x, new_y;
-    var bird = document.getElementById('bird');
-    var slingshot = document.getElementById('slingshot');
+function birdMove(e){
     var x = e.clientX - 25;
     var y = e.clientY - 25;
-    var offsetX = getX(slingshot);
-    var offsetY = getY(slingshot);
+    var offsetX = slingshot.offsetLeft
+    var offsetY = slingshot.offsetTop;
     if(x > offsetX){
         new_x = offsetX;
     }
@@ -40,7 +34,21 @@ function divMove(e){
     else{
         new_y = offsetY + 50;
     }
-    console.log(offsetX, offsetY);
     bird.style.left = new_x + 'px';
     bird.style.top = new_y + 'px';
+    
+    //Calculation for angle and displacement
+    disp = Math.sqrt(Math.pow(offsetX - new_x, 2) + Math.pow(new_y - offsetY, 2));
+    angle = Math.atan((new_y - offsetY)/(offsetX - new_x));
+    angle_deg = angle * (180 / Math.PI);
+}
+function launch(){
+    speed = Math.pow(disp, 2);
+    var sin = Math.sin(angle);
+    var sin_2 = Math.sin(angle * 2);
+    height = (speed * Math.pow(sin, 2)) / (gravity_constant * 2);
+    range = (speed * sin_2) / gravity_constant;
+    console.log("height: ", height);
+    console.log("range: ", range);
+    
 }
